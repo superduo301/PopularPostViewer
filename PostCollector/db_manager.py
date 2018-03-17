b6be21176e5abb78c306ed588385baeedce7c3f2
Superduo301 <superduo301@gmail.com>
2018-03-18 00:01:13 +0900

    add creating database and table

--- PostCollector/db_manager.py
+++ PostCollector/db_manager.py
@@ -1,12 +1,25 @@
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import drop_database
+from sqlalchemy_utils.functions import database_exists, create_database
 
-from models import Post
+from models import Post, Base
 
 print(sqlalchemy.__version__)
 
-engine = create_engine('postgresql+psycopg2://postgres:outback15@localhost/posts_db', echo=True)
+
+engine = create_engine('postgresql+psycopg2://postgres@localhost/posts_db', echo=True)
+
+# init
+drop_database(engine.url)
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+metadata = MetaData()
+Base.metadata.create_all(engine)
+
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -33,3 +46,29 @@ def read():
 
 def close():
     session.close()
+
+
+
+    # -- Table: public.posts
+    #
+    # -- DROP TABLE public.posts;
+    #
+    # CREATE TABLE public.posts
+    # (
+    #     id serial NOT NULL,
+    # source character varying(64) COLLATE pg_catalog."default",
+    #                                                 category character varying(64) COLLATE pg_catalog."default",
+    #                                                                                                   title character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    #                                                                                                                                                                 author character varying(64) COLLATE pg_catalog."default",
+    #                                                                                                                                                                                                                 hit integer,
+    #                                                                                                                                                                                                                     url character varying(2000) COLLATE pg_catalog."default",
+    #                                                                                                                                                                                                                                                                    "time" timestamp with time zone,
+    # CONSTRAINT posts_pkey PRIMARY KEY (id)
+    # )
+    # WITH (
+    #     OIDS = FALSE
+    # )
+    # TABLESPACE pg_default;
+    #
+    # ALTER TABLE public.posts
+    # OWNER to postgres;
--- PostCollector/main.py
+++ PostCollector/main.py
@@ -1,7 +1,5 @@
-import post_collector
-
-from models import Post
 import db_manager
+import post_collector
 
 
 def main():
@@ -18,27 +16,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# -- Table: public.posts
-#
-# -- DROP TABLE public.posts;
-#
-# CREATE TABLE public.posts
-# (
-#     id serial NOT NULL,
-# source character varying(64) COLLATE pg_catalog."default",
-#                                                 category character varying(64) COLLATE pg_catalog."default",
-#                                                                                                   title character varying(256) COLLATE pg_catalog."default" NOT NULL,
-#                                                                                                                                                                 author character varying(64) COLLATE pg_catalog."default",
-#                                                                                                                                                                                                                 hit integer,
-#                                                                                                                                                                                                                     url character varying(2000) COLLATE pg_catalog."default",
-#                                                                                                                                                                                                                                                                    "time" timestamp with time zone,
-# CONSTRAINT posts_pkey PRIMARY KEY (id)
-# )
-# WITH (
-#     OIDS = FALSE
-# )
-# TABLESPACE pg_default;
-#
-# ALTER TABLE public.posts
-# OWNER to postgres;
